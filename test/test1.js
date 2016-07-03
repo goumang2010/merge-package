676d2709b0cb4242673de45d8eaf080b3e635a2f
goumang2010 <goumang2010@live.com>
2016-07-04 00:04:29 +0800

    first test case

---
+++ test/test1.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import path from 'path';
+import execmd from '../execmd.js'
+
+test('rev_merge_test', async (x) => {
+	let argu={
+    	_:['mod1','mod2','mod3'],
+    	t:'target'
+    };
+    console.log(argu);
+    let result=execmd(argu);
+    x.is(result.targetfile,path.resolve('target','package.json'));
+    x.is(result.pkgobj.dependencies.mod3,"^9.9.9");
+});
